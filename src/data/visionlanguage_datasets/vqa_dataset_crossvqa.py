e284fe36adc80f86e23616ee74a23e4b8d5a5244
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-04-30 19:08:46 +0000

    all tests pass

--- src/data/visionlanguage_datasets/vqa_dataset_crossvqa.py
+++ src/data/visionlanguage_datasets/vqa_dataset_crossvqa.py
@@ -49,4 +49,47 @@ class VQADataset(Dataset):
 
         Args:
         data_dir : path containing VQA questions and annotations. Also contains mapping from each answer in set of possible answers to a numerical label
-        images_dataset : instance of MSCOCOImagesDataset, that is used to retrieve the MS-COCO image fo
+        images_dataset : instance of MSCOCOImagesDataset, that is used to retrieve the MS-COCO image for each question
+        split: either train/val split
+
+        Returns:
+        Loads all annotations into self.data, where each item is a single VQA pair
+        """
+
+        self.images_dataset = images_dataset
+        if transform:
+            self.images_dataset.pil_transform = transform
+            self.images_dataset.use_albef = True
+        self.data_dir = data_dir
+        self.encoder_type = encoder_type
+        if split=='test':
+            split = 'test_small'
+        self.split = split
+        self.task_key = task_key
+
+        file_root =  "./data/"
+
+        self.tokenizer = kwargs["tokenizer"] if "tokenizer" in kwargs else None
+        self.label2idxs = {}
+        if "abstract" in task_key:
+            self.questions_file = os.path.join(
+                data_dir, "abstract_{}.json".format(split)
+            )
+            self.annotations_file = os.path.join(
+                data_dir, "abstract_v002_val2015_annotations.json".format(split)
+            )
+            self.ans2label_file = os.path.join(file_root, "abstract/ans2label.pkl")
+        elif "toronto" in task_key:
+            self.annotations_file = os.path.join(
+                data_dir, "toronto_{}.json".format(split)
+            )
+            self.questions_file = os.path.join(
+                data_dir, "toronto_{}.json".format(split)
+            )
+            self.ans2label_file = os.path.join(file_root, "toronto/ans2label.pkl".format(split))
+        elif "art" in task_key:
+            self.annotations_file = os.path.join(file_root, "art/art_{}.json".format(split))
+            self.questions_file = os.path.join(file_root, "art/art_{}.json".format(split))
+            self.ans2label_file = os.path.join(file_root, "art/ans2label_small.pkl".format(split))
+        elif "gqa" in task_key:
+            self.ans2labe
