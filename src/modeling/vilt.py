8ca4f0b3979b049e721f2c47b8e0957e43f415db
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-12 13:42:56 +0000

    asm

--- src/modeling/vilt.py
+++ src/modeling/vilt.py
@@ -11,4 +11,50 @@ from typing import List, Dict
 from typing_extensions import OrderedDict
 
 import numpy as np
-import torc
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformers import BertConfig, BertTokenizer, BertModel
+from transformers import ViltConfig, ViltProcessor, ViltModel
+from transformers import BertTokenizerFast
+from transformers import logging as transformers_logging
+
+from src.modeling.continual_learner import EncoderWrapper, ContinualLearner
+from src.modeling.adaptered_output import Adaptered_ViltOutput
+
+class ViltEncoderWrapper(EncoderWrapper):
+
+    def __init__(self,
+                 processor: ViltProcessor,
+                 vilt: ViltModel,
+                 device: torch.device):
+        """
+        Wrapper around Vilt model from huggingface library
+        this is the class that gets saved during checkpointing for continual learning
+        args:
+        processor - instance of ViltProcessor
+        vilt - instance of ViltModel class
+        device - gpu/cuda
+        """
+
+        super().__init__()
+        self.processor = processor
+        self.vilt = vilt
+        self.device = device
+        # Yao: original:
+        # self.processor.tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+        # Yao: changed to the following:
+        BERT_LOCAL_PATH = './models/bert-base-uncased'
+        self.processor.tokenizer = BertTokenizerFast.from_pretrained(BERT_LOCAL_PATH, local_files_only=True)
+
+        self.max_text_length = self.vilt.config.max_position_embeddings
+        self.encoder_dim = self.vilt.config.hidden_size
+
+        self.expand_modality_type_embeddings()
+
+    def reset_processor(self, max_text_length: int, img_size: tuple):
+        self.max_text_length = max_text_length
+        self.processor.feature_extractor.size = img_size
+
+    def reallocate_text_image(self, pretrained_
