261265bb145cea955b779ea4d52e793e4e2eacf0
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-12 15:27:49 +0000

    Whitespace change.

--- src/modeling/vilt.py
+++ src/modeling/vilt.py
@@ -57,4 +57,42 @@ class ViltEncoderWrapper(EncoderWrapper):
         self.max_text_length = max_text_length
         self.processor.feature_extractor.size = img_size
 
-    def reallocate_text_image(self, pretrained_
+    def reallocate_text_image(self, pretrained_pos_emb: torch.Tensor, max_len: int, img_size: int):  # not used
+        """
+        Re-allocate some of the image token slots to the language position embeddings
+
+        Args:
+        pretrained_pos_emb: Pretrained position embeddings which are extended for creating extra language token slots
+        max_len: new maximum length of language inputs (original is 40 for ViLT)
+        img_size: new size of images, which requires fewer slots so that extra image slots can be allocated to extending language slots
+        """
+        vilt_config = self.vilt.config
+        assert max_len % vilt_config.max_position_embeddings == 0
+
+        self.reset_processor(max_len, img_size)
+
+        # copy the pretrained positional embeddings to support texts with longer max_len
+        extended_pos_emb = torch.cat([pretrained_pos_emb \
+                                      for _ in range(0, max_len, vilt_config.max_position_embeddings)], 0)
+        # extend & re-init Embedding
+        self.vilt.embeddings.text_embeddings.position_embeddings = \
+            nn.Embedding(max_len, vilt_config.hidden_size).from_pretrained(extended_pos_emb, freeze=False)
+
+
+        # extend self.position_ids
+        # https://github.com/huggingface/transformers/blob/main/src/transformers/models/vilt/modeling_vilt.py#L274
+        self.vilt.embeddings.text_embeddings. \
+            register_buffer("position_ids", torch.arange(max_len).expand((1, -1)))
+
+    def process_inputs(self, images: List, texts: List[str]) -> Dict:
+        """
+        Returns encodings that can be inputted to the ViLT transformer
+
+        Args:
+        images - list of PIL Image objects
+        texts - list of text strings
+
+        Returns:
+        encodings - dictionary, where each key corresponds to a different argument of the vilt's forward method
+        """
+        encodin
