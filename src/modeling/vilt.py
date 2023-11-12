9351331fef08fd1c84303b6518fa95838e81d258
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-12 22:12:12 +0000

    Release v0.2.1.

--- src/modeling/vilt.py
+++ src/modeling/vilt.py
@@ -95,4 +95,45 @@ class ViltEncoderWrapper(EncoderWrapper):
         Returns:
         encodings - dictionary, where each key corresponds to a different argument of the vilt's forward method
         """
-        encodin
+        encodings = self.processor(images=images, text=texts, max_length=self.max_text_length,
+                                   padding=True, truncation=True, return_tensors="pt").to(self.device)
+        return encodings
+
+    def expand_modality_type_embeddings(self, type_vocab_size=3):
+        """
+        ViLT contains only 2 token type embeddings - some tasks (like NLVR) require three token type embeddings
+        Method makes a copy second token type embedding into a new third embedding parameter
+        """
+        self.vilt.config.modality_type_vocab_size = type_vocab_size
+        # https://github.com/dandelin/ViLT/blob/762fd3975c180db6fc88f577cf39549983fa373a/vilt/modules/vilt_module.py#L85
+        emb_data = self.vilt.embeddings.token_type_embeddings.weight.data
+        self.vilt.embeddings.token_type_embeddings = nn.Embedding(type_vocab_size, self.encoder_dim)
+        self.vilt.embeddings.token_type_embeddings.weight.data[0, :] = emb_data[0, :]
+        self.vilt.embeddings.token_type_embeddings.weight.data[1, :] = emb_data[1, :]
+        self.vilt.embeddings.token_type_embeddings.weight.data[2, :] = emb_data[1, :]
+
+    def forward(self, **encodings: Dict) -> torch.FloatTensor:
+        """
+        Does forward pass of input encodings through ViltModel
+        https://huggingface.co/docs/transformers/model_doc/vilt#transformers.ViltModel.forward
+
+        Args:
+        encodings: Dictionary containing inputs ViltModel's forward pass
+
+        Returns:
+        pooler_output: torch.FloatTensor of size (batch_size, hidden_size)
+        """
+
+        output = self.vilt(**encodings)
+
+        return output.pooler_output
+
+
+
+    def freeze_all_weights(self):
+        """
+        Freeze all parameters in self.vilt
+        """
+
+        for p in self.vilt.parameters():
+         
