1f3e2b2bb300c0e7bacf368224da8442649b5a7a
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-25 06:27:26 +0000

    Create todo.md

--- src/train/visionlanguage_tasks/train_vqa.py
+++ src/train/visionlanguage_tasks/train_vqa.py
@@ -36,4 +36,47 @@ logging.basicConfig(
 class VQATrainer(TaskTrainer):
 
     def __init__(self,
-    
+                 logger,
+                 args: argparse.Namespace,
+                 task_configs: Dict,
+                 model_config: Dict,
+                 device: torch.device,
+                 task_key,
+                 task_output_dir,
+                 accelerator):
+
+        '''
+        Initializes a Trainer that handles training of a model on the VCR task
+
+        args: Arguments provided by user
+        task_configs: dictionary containing task-specific configuration parameters for all tasks
+        model_config: dictionary containing model-specific configuration parameters
+        device: cuda/cpu
+        '''
+
+        super().__init__()
+
+        self.args = args
+        self.local_epochs = args.local_epochs
+        self.device = device
+        self.accelerator = accelerator
+        self.task_output_dir = task_output_dir
+        self.task_key = task_key
+
+        self.vqa_config = task_configs['vqa']
+        self.data_dir = os.path.join(args.climb_data_dir, self.vqa_config['data_dir'])
+
+        # Model-specific stuff
+        self.visual_input_type = model_config['visual_input_type']
+        self.batch2inputs_converter = model_config['batch2inputs_converter']
+
+        # Load COCO Images dataset for image data backbone
+        images_source = self.vqa_config['images_source']
+        mscoco_config = task_configs[images_source]
+        self.images_dataset = MSCOCOImagesDataset(coco_dir=os.path.join(args.climb_data_dir, mscoco_config['data_dir']),
+                                                  visual_input_type=args.visual_input_type)
+
+        # Create dataloaders for training and validation
+        self.vqa_train_dataloader = build_vqa_dataloader(args=args,
+                                                    data_dir=self.data_dir,
+                                                    images_dataset=self.images_dataset,
