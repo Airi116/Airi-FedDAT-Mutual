c956c98d84c2db4ca7911bf32cf23b372057762d
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-05-06 12:56:17 +0000

    Code linting

--- src/utils/make_table.py
+++ src/utils/make_table.py
@@ -42,3 +42,30 @@ def merge_all_results(all_scores, fns, backbone):
 def dump_outputs(all_scores, task_name):
     out_fn = f"{task_name}.json"
     with open(out_fn, "w") as outfile:
+        outfile.write(json.dumps(all_scores))
+    with open(out_fn, "r") as f:
+        rdict = json.load(f)
+
+    pp = pprint.PrettyPrinter()
+    pp.pprint(rdict)
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "input task name"
+    task_name = sys.argv[1]
+    tree = lambda: defaultdict(tree)
+    all_scores = tree()
+
+    if task_name in ['coco', 'imagenet', 'inat2019', 'places365']:
+        dir_name = 'vision_only'
+        fns = glob.glob(f"/data/experiments/MCL/{dir_name}/{task_name}_*")
+        all_scores = merge_all_results(all_scores, fns, 'ViLT')
+    else:
+        dir_name = 'lang_only'
+
+        fns = glob.glob(f"/data/experiments/MCL/{dir_name}/{task_name}_*")
+        all_scores = merge_all_results(all_scores, fns, 'ViLT')
+        fns = glob.glob(f"/data/experiments/MCL/{dir_name}/viltbert/{task_name}_*")
+        all_scores = merge_all_results(all_scores, fns, 'ViLTBERT')
+
+    dump_outputs(all_scores, task_name)
